f73150461dc58019ea1eaa3c4c625efbf6545012
Byron Mann <byronosity@gmail.com>
2019-02-27 14:31:37 -0700

    relase 0.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as f:
 
 setup(
     name='requests-sigv4',
-    version='0.1.2',
+    version='0.1.3',
     packages=['requests_sigv4'],
     url='https://github.com/cleardataeng/requests-sigv4',
     license='Apache 2.0',
