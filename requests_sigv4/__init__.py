e932f576c7c61e71c6bff785baf2ee3f1fb1c024
Byron Mann <byronosity@gmail.com>
2019-03-12 15:11:47 -0700

    python 3.4 urlencode signature

--- requests_sigv4/__init__.py
+++ requests_sigv4/__init__.py
@@ -13,8 +13,10 @@ is_py3 = (_ver[0] == 3)
 if is_py2:
     import urllib
     urllib.quote_plus = urllib.quote
-else:
+
+if is_py3:
     from copy import deepcopy
+    import urllib.parse
     from urllib.parse import quote, urlencode
     from requests_sigv4.sigv4request import requests
     import requests_aws_sign
@@ -23,9 +25,14 @@ else:
 
     # patching urlencode in python3 as it has a default
     # quote_via=quote_plus as the default
-    def new_urlencode(query, doseq=False, safe='',
-                      encoding=None, errors=None, quote_via=None):
-        return _urlencode(
-            query, doseq, safe, encoding, errors, quote)
-    requests.models.urlencode = new_urlencode
-    requests_aws_sign.requests_aws_sign.urlencode = new_urlencode
+    if sys.version_info[1] == 4:
+        urllib.parse.quote_plus = quote
+    else:
+        def new_urlencode(
+            query, doseq=False, safe='', encoding=None, errors=None,
+                quote_via=None):
+            return _urlencode(
+                query, doseq, safe, encoding, errors, quote)
+
+        requests.models.urlencode = new_urlencode
+        requests_aws_sign.requests_aws_sign.urlencode = new_urlencode
